11c88cfc2e93f240ecec6781bda776a9d83c5094
Masum Billal <proafxin@users.noreply.github.com>
2025-03-25 13:51:40 +0600

    added search by author title year publiser journal

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="Reffer")
+st.title("Reffer")
+st.subheader("A community based open source reference solution.")
+st.write(
+    "Reffer is a community based open source reference solution that allows you to store, search and add your references."
+)
--- api/services/parser.py
+++ api/services/parser.py
@@ -3,13 +3,13 @@ import bibtexparser
 from bibtexparser import middlewares as mw
 
 
-def parse_bibtext(text: str) -> list[dict[str, str | list[str]]]:
+def parse_bibtext(text: str) -> list[dict[str, str]]:
     library = bibtexparser.parse_string(
         text,
         append_middleware=[mw.LatexDecodingMiddleware(), mw.MonthIntMiddleware(True)],
     )
 
-    bibentries: list[dict[str, str | list[str]]] = []
+    bibentries: list[dict[str, str]] = []
     for entry in library.entries:
         bibentry = {}
         bibentry["entry_type"] = entry.entry_type
@@ -17,9 +17,6 @@ def parse_bibtext(text: str) -> list[dict[str, str | list[str]]]:
         for field in entry.fields:
             key = field.key.lower()
             value = field.value
-
-            if key == "author":
-                value = [token.strip() for token in value.split(" and ")]
 
             bibentry[key] = value
 
